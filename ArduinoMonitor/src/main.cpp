1d16f362b5a69a66f5459fbfa75615cb3a0b176a
JL <jan.loeliger@gmail.com>
2023-06-22 14:11:59 +0200

    basefiles

---
+++ ArduinoMonitor/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include "HCSR04.h"
+
+float getDistance();
+
+UltraSonicDistanceSensor distanceSensor(13, 12);  // Initialize sensor that uses digital pins 13 and 12.
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+getDistance();
+}
+
+float getDistance() {
+  double distance = distanceSensor.measureDistanceCm();
+
+  Serial.print(F("°C - Distance: "));
+  Serial.print(distance);
+  Serial.println(F("cm"));
+  delay(1000);
+  return 0;
+}
