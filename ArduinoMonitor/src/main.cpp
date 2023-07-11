aacddaea0e0b34cacb0a0f8092071c6a6fa8b4ba
jl <jan.loeliger@gmail.com>
2023-07-11 06:04:15 +0200

    improved code

--- ArduinoMonitor/src/main.cpp
+++ ArduinoMonitor/src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// constants
+const int SERVO_MAX_POS = 120;
+const int SERVO_MIN_POS = 45;
+const int MASTER_ADDRESS = 1;
+const int ARDUINO_ADDRESS = 3;
+
+// Pins
+const int SERVO_ENTRY_PIN = 11;
+const int SERVO_EXIT_PIN = 10;
+const int BUTTON_ENTRY_PIN = 9;
+const int BUTTON_EXIT_PIN = 8;
+const int SDA_PIN = 19;
+const int SCL_PIN = 18;
+
+Servo servoEntry; 
+Servo servoExit;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // inits servos
+  servoEntry.attach(SERVO_ENTRY_PIN);
+  servoExit.attach(SERVO_EXIT_PIN);
+  servoEntry.write(SERVO_MIN_POS);
+  servoExit.write(SERVO_MIN_POS);
+  // sets buttons
+  pinMode(BUTTON_ENTRY_PIN, INPUT);
+  pinMode(BUTTON_EXIT_PIN, INPUT);
+  // inits i2c
+  Wire.begin(ARDUINO_ADDRESS);
+  Wire.onReceive(receiveEvent); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(digitalRead(BUTTON_ENTRY_PIN) == 1) {
+    servoEntry.write(SERVO_MAX_POS);
+    delay(200);
+    servoEntry.write(SERVO_MIN_POS);
+  }
+
+    if(digitalRead(BUTTON_EXIT_PIN) == 1) {
+    servoExit.write(SERVO_MAX_POS);
+    delay(200);
+    servoExit.write(SERVO_MIN_POS);
+  }
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void receiveEvent(int numBytes) {
+  while (Wire.available()) {
+    int parkingCount = Wire.read();
+    
 }
--- Core2/src/main.cpp
+++ Core2/src/main.cpp
@@ -1,22 +1,66 @@
+#include <M5Core2.h>
+#include <Arduino.h>
 #include <HCSR04.h>
+
+// functions
+void drawDisplay(int, int);
+bool isParked(int);
+
+// constants
+const int FONT_SIZE = 20;
+const int SCREEN_WIDTH = 320;
+const int SCREEN_HEIGHT = 240;
+const int PADDING = 80;
+const int CHECK_INTERVAL = 500;  // Interval for checking task in milliseconds
 
 byte triggerPin = 13;
 byte echoPin = 14;
 
-void setup () {
+unsigned long previousCheckTime = 0;
+u_int8_t reservedParkCount;
+
+void setup() {
+  M5.begin();
   Serial.begin(9600);
   HCSR04.begin(triggerPin, echoPin);
 }
 
-void loop () {
-  float temperature = 15;
+void loop() {
+  unsigned long currentMillis = millis();
+
+  // Checking Task
+  if (currentMillis - previousCheckTime >= CHECK_INTERVAL) {
+    previousCheckTime = currentMillis;
+    double* distances = HCSR04.measureDistanceCm();
+    bool isLeftParked = isParked(distances[0]);
+
+    // Drawing Task
+    drawDisplay(isLeftParked, 0);
+  }
+
+
+}
+
+void drawDisplay(int leftFreeParkingCount, int rightFreeParkingCount) {
+  // left parking spot
+  M5.Lcd.setCursor(PADDING, FONT_SIZE);
+  M5.Lcd.setTextSize(FONT_SIZE);
+  M5.Lcd.print(leftFreeParkingCount);
+  M5.Lcd.setCursor(FONT_SIZE, FONT_SIZE * 4);
+  M5.Lcd.print("<--");
   
-  double* distances = HCSR04.measureDistanceCm(temperature);
-  
-  Serial.print("1: ");
-  Serial.print(distances[0]);
-  Serial.println(" cm");
-  
-  Serial.println("---");
-  delay(250);
+  // right parking spot
+  M5.Lcd.setCursor(SCREEN_WIDTH - PADDING - FONT_SIZE, SCREEN_HEIGHT - PADDING - FONT_SIZE * 4);
+  M5.Lcd.setTextSize(FONT_SIZE);
+  M5.Lcd.print(rightFreeParkingCount);
+  M5.Lcd.setCursor(SCREEN_WIDTH / 2, SCREEN_HEIGHT - PADDING - FONT_SIZE);
+  M5.Lcd.print("-->");
+}
+
+bool isParked(int distance) {
+  if(distance <= 8) {
+    return true;
+  } else {
+    return false;
+  }
 }
